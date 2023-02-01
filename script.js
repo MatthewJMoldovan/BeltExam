76c5c1c5114f8b7bde78eb1db624d3e832ac1abc
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-02-01 13:59:32 -0700

    Succulent_Shop

---
+++ script.js
@@ -0,0 +1,13 @@
+var cookies = document.querySelector("#cookieCard");
+
+function acceptCookies(element){
+    element.remove();
+}
+
+function over(element){
+    element.src = "/images/assets/succulents-2.jpg"
+}
+
+function out(element){
+    element.src= "/images/assets/succulents-1.jpg"
+}
